6875668efc82e9af386d106f85f534bbc06a8db6
Fylybg0 <bodorfilip8@gmail.com>
2023-12-05 13:13:43 +0100

    premenovanie suborov

--- 4/1.py
+++ 4/1.py
@@ -1,12 +1,10 @@
 f = [[[int(y) for y in x.split()] for x in i.split(': ')[1].split(' | ')] for i in open('input.txt').read().split('\n')]
-a = [1] * len
 c = 0
 
 for i in f:
     s = 0
     for x in i[1]:
         if x in i[0]:
-            print(i[0], x)
             s += 1
     if s != 0:
         c += 2 ** (s - 1)
--- 4/2.py
+++ 4/2.py
@@ -2,11 +2,9 @@ f = [[[int(y) for y in x.split()] for x in i.split(': ')[1].split(' | ')] for i 
 a = [1] * len(f)
 
 for i in range(len(f)):
-    print(a)
     s = 0
     for x in f[i][1]:
         if x in f[i][0]:
-            print(f[i][0], x)
             s += 1
     if s != 0:
         for z in range(s):
