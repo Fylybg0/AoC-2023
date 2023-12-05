cc4bf4950f659912c6a5e4d1cad968ce2f0814be
Fylybg0 <bodorfilip8@gmail.com>
2023-12-05 13:10:15 +0100

    day 5 part 2 fixovanie erroru 3 roky asi

--- 5/1.2.py
+++ 5/1.2.py
@@ -3,24 +3,19 @@ a = (lambda y: [(int(y[i]), int(y[i]) + int(y[i+1]) - 1) for i in range(0, len(y
 f = [(lambda y: [x for x in y[0][:-5].split('-to-')] + [[int(z) for z in x.split()] for x in y[1:]])(i.split('\n')) for i in f[1:]]
 fd = {(i[0], i[1]): i[2:] for i in f}
 fg = {i: x for i, x in fd.keys()}
-print(f)
-
 
 best = float('inf')
-                                    
+
 def overlap(x, y):      
     if (x[1] >= y[1] and x[0] <= y[0]):
         return y, ([(x[0], y[0]-1)] if x[0] != y[0] else []) + ([(y[1]+1, x[1])] if y[1] != x[1] else [])
     elif (x[0] >= y[0] and x[1] <= y[1]):
         return x, []
     elif (x[0] <= y[0] <= x[1] and x[1] <= y[1]):
-        #print(x, y, (y[0], x[1]), [(x[0], y[0] - 1)] if x[0] != y[0] else [])
         return (y[0], x[1]), [(x[0], y[0] - 1)] if x[0] != y[0] else []
     elif (x[0] >= y[0] and x[0] <= y[1] <= x[1]):
-        #print(x, y, (x[0], y[1]), [(y[1] + 1, x[1])]  if x[1] != y[1] else [])
         return (x[0], y[1]), [(y[1] + 1, x[1])]  if x[1] != y[1] else []
 
-print(a)
 for i in range(len(a)):
     current = 'seed'
     ranges = [a[i]]
@@ -33,14 +28,13 @@ for i in range(len(a)):
                     new.append((over[0][0] - y[1] + y[0], over[0][1] - y[1] + y[0]))
                     ranges += over[1].copy()
                     ranges.pop(0)
-                    print(over, current)
                     break
             else:
-                new += ranges.copy()
-                ranges = []
+                new += [ranges[0]]
+                ranges.pop(0)
         current = fg[current]
         ranges = new.copy()
-    
-    print(min(ranges, key=lambda x: x[0]))
+        
     best = min(best, min(ranges, key=lambda x: x[0])[0])
+
 print(best)
